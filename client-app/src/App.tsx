4f9873976aa6358d91b306ef0f27f943e9da0004
Noah Maktabi <mctabi@me.com>
2021-06-15 16:09:32 +0200

    React installed. Semantic UI installed. Axios installed

---
+++ client-app/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:5001/api/activities').then(response => {
+    console.log(response);
+      
+    setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Event-Partner' />
+      <List>
+      {activities.map((activity: any) => (
+            <List.Item key={activity.id} >
+              {activity.title}
+            </List.Item>
+          ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
