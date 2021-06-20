0708acf486d3a97f5ccddbffae5a70134c9402fc
Noah Maktabi <mctabi@me.com>
2021-06-20 09:01:46 +0200

    React local crud dont

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -27,7 +27,7 @@ namespace Application.Activities
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Activity.Id, cancellationToken);
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
                 
                 _mapper.Map(request.Activity, activity);
                 
