cde52806bf57a09646c224a12ce6afa17908c76d
Noah Maktabi <mctabi@me.com>
2021-06-28 20:05:16 +0200

    Styling to UI

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,13 @@
 import { observer } from "mobx-react-lite";
 import React, { useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import { Button, Card, Image } from "semantic-ui-react";
+import {  useParams } from "react-router-dom";
+import {  Grid} from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import ActivityDetailedChat from "./ActivityDetailedChat";
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from "./ActivityDetailedInfo";
+import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
 
 
 
@@ -19,7 +23,21 @@ export default observer( function ActivityDetails() {
     if (loadingInitial || !activity) return <LoadingComponent /> ;
 
     return (
-        <Card fluid>
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityDetailedHeader activity={activity} />
+                <ActivityDetailedInfo activity={activity} />
+                <ActivityDetailedChat />
+            </Grid.Column>
+            <Grid.Column width={6} >
+                <ActivityDetailedSidebar />
+            </Grid.Column>
+        </Grid>
+    )
+})
+
+/*
+<Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`}  />
             <Card.Content>
                 <Card.Header>{activity.title}</Card.Header>
@@ -37,5 +55,4 @@ export default observer( function ActivityDetails() {
                 </Button.Group>
             </Card.Content>
         </Card>
-    )
-})
+*/
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,13 +1,19 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import {  Container, Header, Segment, Image, Button } from "semantic-ui-react";
 
 export default observer(function HomePage() {
     return (
-        <Container style={{margingTop: '7em'}} >
-            <h1>Home Page</h1>
-            <h3>Go To <Link to='/activities' >Activities</Link> </h3>
-        </Container>
+        <Segment inverted textAlign='center' vertical className='masthead' >
+            <Container text >
+                <Header as='h1' inverted >
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
+                    Event-Partner
+                </Header>
+                <Header as='h2' inverted content='Welcome to Event-Partner' />
+                <Button as={Link} to='/activities' size='huge' inverted >Take me to the activities</Button>
+            </Container>
+        </Segment>
     )
 });
