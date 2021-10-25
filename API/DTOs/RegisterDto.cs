4e63af990648c9711c19df69bdaedae6a858a3c9
Noah Maktabi <61517594+noah11m@users.noreply.github.com>
2021-10-25 11:46:16 +0200

    Update RegisterDto.cs

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -14,7 +14,7 @@ namespace API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         [Required]
         public string Username { get; set; }
